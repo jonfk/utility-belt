8c077d4d7378d3c6f022caa4c03f28809a5bb0e5
Jonathan Fok kan <jfokkan@gmail.com>
2016-01-31 17:31:21 -0500

    inspection-server: Removed unused code

--- inspection-server/inspection-server.go
+++ inspection-server/inspection-server.go
@@ -15,10 +15,6 @@ import (
 func main() {
 	http.HandleFunc("/", handler)
 
-	// http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	// })
-
 	fmt.Println("serving on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
@@ -38,29 +34,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	ioutil.WriteFile("temp.txt", buf.Bytes(), 0777)
 	fmt.Fprintf(w, "ok printed")
-
-	// newURL, err := url.Parse("https://www.strava.com/api/v3/uploads")
-	// if err != nil {
-	// 	log.Println(err)
-	// }
-
-	// newReq, err := http.ReadRequest(bufio.NewReader(strings.NewReader(buf.String())))
-	// newReq.RequestURI = ""
-	// newReq.URL = newURL
-
-	// client := &http.Client{}
-	// resp, err := client.Do(newReq)
-	// if err != nil {
-	// 	log.Println(err)
-	// }
-	// log.Println("response: ")
-	// spew.Dump(resp)
-
-	// if resp != nil {
-	// 	respBuf := new(bytes.Buffer)
-	// 	respBuf.ReadFrom(resp.Body)
-	// 	respBody := respBuf.String()
-	// 	fmt.Println(respBody)
-	// }
 
 }
