2726896c33ffa1d5e12f344bb9c0417afd950b99
Jonathan Fok kan <jfokkan@gmail.com>
2016-09-16 11:51:41 -0400

    daily-uptime: Added utility to log uptime every time the user logs out or shutsdown

---
+++ sh/daily-uptime/daily-uptime-osx.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+#
+# Reference:
+# - http://launchd.info/
+# - http://stackoverflow.com/questions/16542301/running-a-shell-script-on-shutdown-via-launchd
+#
+# To Do to run as work:
+# - cp daily-uptime-osx.sh ~/Applications
+# - Edit the path in daily-uptime-osx.plist to path of script
+# - cp daily-uptime-osx.plist ~/Library/LaunchAgents
+# - logout and login
+#
+
+DAILYUPTIMEFILE="$HOME/daily-uptime.log"
+
+StopService() {
+    echo "Shutting Down. Logging your uptime :)"
+    touch "$DAILYUPTIMEFILE"
+    uptime >> "$DAILYUPTIMEFILE"
+    exit 0
+}
+
+trap StopService SIGTERM
+while true; do
+    sleep 86400 &
+    wait
+done
