aec974d93e83c84f7f44e9a0284ba85e32026f2a
Jonathan Fok kan <jfokkan@gmail.com>
2016-09-16 18:02:49 -0400

    daily-uptime: added launchctl command to install.sh

--- sh/daily-uptime/install.sh
+++ sh/daily-uptime/install.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+set -e
+set -x
+
 INSTALL_PATH_JOB="$HOME/Library/LaunchAgents/daily-uptime-osx.plist"
 INSTALL_PATH_SCRIPT="$HOME/Applications/daily-uptime-osx.sh"
 
 cp daily-uptime-osx.plist "$INSTALL_PATH_JOB"
 cp daily-uptime-osx.sh  "$INSTALL_PATH_SCRIPT"
+launchctl load -w "$INSTALL_PATH_JOB"
