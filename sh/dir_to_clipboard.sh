6291d3a48171d803716434bb418f2d73ef488a18
Jonathan Fok kan <jonathan@fokkan.ca>
2024-10-10 13:08:00 -0400

    sh: Added dir_to_clipboard.sh

---
+++ sh/dir_to_clipboard.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+usage() {
+    echo "Usage: $0 [-h] [-e exclude_pattern]"
+    echo "  -h: Display this help message"
+    echo "  -e exclude_pattern: Exclude files matching this pattern"
+    exit 1
+}
+
+exclude_pattern=""
+
+while getopts "he:" opt; do
+    case ${opt} in
+        h )
+            usage
+            ;;
+        e )
+            exclude_pattern=$OPTARG
+            ;;
+        \? )
+            usage
+            ;;
+    esac
+done
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    clipboard_cmd="pbcopy"
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    if ! command -v xclip &> /dev/null; then
+        echo "Error: xclip is not installed. Please install it using your package manager."
+        exit 1
+    fi
+    clipboard_cmd="xclip -selection clipboard"
+else
+    echo "Unsupported operating system"
+    exit 1
+fi
+
+find_cmd="find . -type f"
+if [ -n "$exclude_pattern" ]; then
+    find_cmd="$find_cmd -not -path '$exclude_pattern'"
+fi
+
+$find_cmd -print0 | xargs -0 -I {} bash -c "echo -e '\n--- {} ---\n' && cat '{}'" | $clipboard_cmd
+
+echo "File contents copied to clipboard"
