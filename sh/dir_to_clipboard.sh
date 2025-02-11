39a9ebfa2212b4212a8fec6c70aacef475ebe3eb
Jonathan Fok kan <jonathan@fokkan.ca>
2025-02-10 19:30:58 -0500

    Updated dir to clipboard

--- sh/dir_to_clipboard.sh
+++ sh/dir_to_clipboard.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 usage() {
-    echo "Usage: $0 [-h] [-e exclude_pattern]"
+    echo "Usage: $0 [-h] [-e exclude_pattern] [-i include_pattern]"
     echo "  -h: Display this help message"
     echo "  -e exclude_pattern: Exclude files matching this pattern"
+    echo "  -i include_pattern: Include only files matching this pattern"
     exit 1
 }
 
 exclude_pattern=""
+include_pattern=""
 
-while getopts "he:" opt; do
+while getopts "he:i:" opt; do
     case ${opt} in
         h )
             usage
             ;;
         e )
             exclude_pattern=$OPTARG
+            ;;
+        i )
+            include_pattern=$OPTARG
             ;;
         \? )
             usage
@@ -39,6 +44,9 @@ fi
 find_cmd="find . -type f"
 if [ -n "$exclude_pattern" ]; then
     find_cmd="$find_cmd -not -path '$exclude_pattern'"
+fi
+if [ -n "$include_pattern" ]; then
+    find_cmd="$find_cmd -path '$include_pattern'"
 fi
 
 $find_cmd -print0 | xargs -0 -I {} bash -c "echo -e '\n--- {} ---\n' && cat '{}'" | $clipboard_cmd
