74d5dd11a1f0bb17defb72b4a70a7270fcb1fe03
Jonathan Fok kan <jonathan@fokkan.ca>
2025-11-13 09:51:38 -0500

    clean slate git-worktree

--- python/git-smart-worktree/main.py
+++
@@ -1,9 +0,0 @@
-from git_smart_worktree.cli import app
-
-
-def main() -> None:
-    app()
-
-
-if __name__ == "__main__":
-    main()
---
+++ python/git-worktree-utils/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from git-worktree-utils!")
+
+
+if __name__ == "__main__":
+    main()
